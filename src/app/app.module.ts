db47cfc2911c6107ac37e30624942ac0b2491f49
victoriajdias <mvictoriadiasj22@gmail.com>
2026-02-04 12:32:22 -0300

    refactor(tasks): mover módulo tasks para src understanding de arquitetura NestJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { TaskModule } from "./tasks/task.module";
+import { TaskModule } from "../tasks/task.module";
 import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
