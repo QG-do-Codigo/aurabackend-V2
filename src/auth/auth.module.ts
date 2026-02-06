ec659d6853e421f3e4ffc6e5c0cd3609d59b1193
Geana-Almeida <geana.almeida2000@gmail.com>
2026-02-06 16:33:09 -0300

    feat(auth): enhance authentication module with JWT support and user authorization
    - Added JWT configuration and integration for token generation and validation.
    - Implemented AuthTokenGuard for route protection using JWT.
    - Updated AuthService to include token generation during user sign-in.
    - Enhanced UserService to include user authorization checks for CRUD operations.
    - Introduced new DTOs for token payload and constants for request handling.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,29 @@ import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { HashingServiceProtocol } from './hash/hashing.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { BcryptService } from './hash/bcrypt.service';
+import jwtConfig from './config/jwt-config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Global()
 @Module({
+  imports: [
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+
+  ],
   providers: [{
       provide: HashingServiceProtocol,
       useClass: BcryptService
     },
     AuthService
   ],
-  controllers: [AuthController],
   exports: [
-    HashingServiceProtocol
-  ]
+    HashingServiceProtocol,
+    JwtModule,
+    ConfigModule
+  ],
+  controllers: [AuthController]
 })
 export class AuthModule {}
